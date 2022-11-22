c2d335b01ea351222f739f2675077dd7a230721f
Floopinstein <98966948+Floopinstein@users.noreply.github.com>
2022-11-22 11:00:33 +0000

    Started Making Binary Tree

---
+++ ADS_2022_ICA2_OW_ET/ADS_2022_ICA2_OW_ET/Source.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+
+template <class A>
+
+class BinaryTree { A user_id, A CurrentRanking, A FirstName, Surname, Gender, DOB, email, phone, postcode, addres, country, LastLogOnDate, LastLogOnTime, JoinedOn, AccountCredit };
+
